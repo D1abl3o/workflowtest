50c5d50fc593da7e0c34a14f85aa6a413086711d
Dominik Vinan <Dominik.Vinan@bmw.de>
2018-03-27 14:36:37 +0200

    Initial commit.

---
+++ src/workflowtest/HelloBMWorld.java
@@ -0,0 +1,10 @@
+package workflowtest;
+
+public class HelloBMWorld {
+	public static void main(String[] args){
+		System.out.print("Hello BMWorld");
+		System.out.println("");
+		
+		return;	
+	}
+}
