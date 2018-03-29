46eb1b2778073c5ecb753cc6780830f91a9d40cf
Dominik Vinan <Dominik.Vinan@bmw.de>
2018-03-29 14:03:39 +0200

    Replace Inner Loop with error message

--- src/workflowtest/HelloBMWorld.java
+++ src/workflowtest/HelloBMWorld.java
@@ -2,9 +2,7 @@ package workflowtest;
 
 public class HelloBMWorld {
 	public static void main(String[] args){
-		System.out.print("Hello BMWorld");
-		for(int i = 0; i < 3; i++){
-			System.out.println("Tralalala");
-		}
+		System.out.println("Hello BMWorld");
+		System.err.println("Much simpler error message");
 	}
 }
