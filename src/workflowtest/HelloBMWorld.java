98f397642d3e19d97fe403c2a0f3007a0e356c15
Dominik Vinan <Dominik.Vinan@bmw.de>
2018-03-29 14:02:22 +0200

    Add Innerloop to output

--- src/workflowtest/HelloBMWorld.java
+++ src/workflowtest/HelloBMWorld.java
@@ -4,7 +4,8 @@ public class HelloBMWorld {
 	public static void main(String[] args){
 		System.out.print("Hello BMWorld");
 		for(int i = 0; i < 3; i++){
-			System.out.println("Tralalala");
+			for(int a = 3; a >= 0; a--)
+				System.out.println("Tralalala");
 		}
 	}
 }
