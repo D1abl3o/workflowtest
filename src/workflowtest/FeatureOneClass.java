21aa53aedca07a4f7ee585628d136b83a7f414ae
Dominik Vinan <Dominik.Vinan@bmw.de>
2018-04-04 12:46:46 +0200

    Add featureOneClass

---
+++ src/workflowtest/FeatureOneClass.java
@@ -0,0 +1,12 @@
+package workflowtest;
+
+public class FeatureOneClass {
+	String input;
+	public FeatureOneClass(String input){
+		this.input = input;
+	}
+	
+	void output(){
+		System.out.println("FeatureOneClass");
+	}
+}
